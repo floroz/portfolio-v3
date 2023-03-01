7857331df0ee69f50992b84b14ebbcfe18467744
Daniele Tortora <tortora87@gmail.com>
2023-03-01 20:10:08 +0100

    feat: add commitlint and husky

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable unicorn/prefer-module */
+/** eslint-env node */
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "chore",
+        "style",
+        "refactor",
+        "ci",
+        "test",
+        "revert",
+        "perf",
+        "build",
+      ],
+    ],
+  },
+};
