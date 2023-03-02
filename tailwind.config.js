f0e3d004e12493a2940dea84082020d6d0a37fba
Daniele Tortora <danieletortora.contact@gmail.com>
2023-03-02 09:55:05 +0100

    feat: add tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable unicorn/prefer-module */
+/** eslint-env node */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
