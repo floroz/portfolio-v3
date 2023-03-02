9517bf9e018d3e5fbebb4804b29bf4184ea746f9
Daniele Tortora <tortora87@gmail.com>
2023-03-02 10:23:11 +0100

    chore: setup update prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  vueIndentScriptAndStyle: true,
+};
