5d8c5ebb46fb3ada053b1da38a907455054f8feb
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2023-03-02 17:31:01 +0100

    feat: load svg as components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export default defineNuxtConfig({
-  modules: ["nuxt-icon", "@nuxt/image-edge"],
+  modules: ["nuxt-icon", "@nuxt/image-edge", "nuxt-svgo"],
   css: ["~/assets/css/fonts.css", "~/assets/css/main.css"],
   postcss: {
     plugins: {
