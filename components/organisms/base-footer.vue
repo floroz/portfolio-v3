a6fb5613a41365c6d185b39737c7f594ed3a0a6c
Daniele Tortora <danieletortora.contact@gmail.com>
2023-03-03 09:03:59 +0100

    feat: add footer and company carousel

---
+++ components/organisms/base-footer.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import SocialLinks from "~/components/molecules/social-links.vue";
+</script>
+
+<template>
+  <footer class="header-styled border-t border-zinc-700 flex items-center">
+    <div class="flex justify-between items-center w-full">
+      <div class="text-primary">
+        <p>
+          © 2023 All rights reserved.
+          <span class="hidden sm:inline-block"
+            >Design by
+            <NuxtLink
+              to="https://dribbble.com/Kawsarvy"
+              class="underline"
+              >Kawsar Ahmed</NuxtLink
+            ></span
+          >
+        </p>
+      </div>
+      <ul class="flex items-center gap-x-6">
+        <SocialLinks />
+      </ul>
+    </div>
+  </footer>
+</template>
--- components/organisms/base-header.vue
+++ components/organisms/base-header.vue
@@ -1,28 +1,8 @@
 <script setup lang="ts">
-  const LINKS = [
-    {
-      id: "linkedin",
-      iconName: "ant-design:linkedin-filled",
-      href: "https://www.linkedin.com/in/danieletortora/",
-      text: "LinkedIn",
-    },
-    {
-      id: "GitHub",
-      iconName: "ant-design:github-filled",
-      href: "https://github.com/floroz",
-      text: "GitHub",
-    },
-    {
-      id: "Twitter",
-      iconName: "ant-design:twitter-circle-filled",
-      href: "https://twitter.com/floroz87",
-      text: "Twitter",
-    },
-  ];
+  import SocialLinks from "~/components/molecules/social-links.vue";
 </script>
-
 <template>
-  <header class="max-w-4xl p-4 h-20 border-b border-b-zinc-800 m-auto">
+  <header class="header-styled border-b border-zinc-700">
     <nav>
       <ul class="flex items-center gap-x-6 w-full h-full">
         <li class="text-primary flex-grow">
@@ -42,30 +22,7 @@
             </div>
           </NuxtLink>
         </li>
-        <li
-          v-for="(link, index) in LINKS"
-          :key="link.id"
-          class="cursor-pointer text-primary hover:text-zinc-200"
-        >
-          <NuxtLink
-            :to="link.href"
-            :aria-label="link.text"
-            class="flex items-center gap-x-4"
-            target="_blank"
-          >
-            <Icon
-              class="sm:!hidden"
-              :name="link.iconName"
-              size="24"
-            ></Icon>
-            <span class="hidden sm:inline">{{ link.text }}</span>
-            <span
-              v-if="index !== LINKS.length - 1"
-              class="hidden sm:inline"
-              >/</span
-            >
-          </NuxtLink>
-        </li>
+        <SocialLinks />
       </ul>
     </nav>
   </header>
