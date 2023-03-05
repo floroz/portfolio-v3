e95209375a265ed97d5fd2e54d747b2c72def4f9
Daniele Tortora <danieletortora.contact@gmail.com>
2023-03-05 12:40:37 +0100

    fix: remove nuxt/image plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export default defineNuxtConfig({
-  modules: ["nuxt-icon", "@nuxt/image-edge", "nuxt-svgo"],
+  // removed @nuxt/image-edge until this is fixed https://github.com/nuxt/image/issues/689
+  // modules: ["nuxt-icon", "@nuxt/image-edge", "nuxt-svgo"],
+  modules: ["nuxt-icon", "nuxt-svgo"],
   css: ["~/assets/css/fonts.css", "~/assets/css/main.css"],
   postcss: {
     plugins: {
@@ -8,8 +10,5 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  image: {
-    dir: "assets/images",
-    provider: process.env.VERCEL_ENV ? "vercel" : "ipx",
-  },
+  // image: {},
 });
