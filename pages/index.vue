9f94c63a712f1ee5889750bc9c71d1a1dd363e4d
Daniele Tortora <tortora87@gmail.com>
2023-03-05 19:12:46 +0100

    feat: site metadata and tags

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import HeroSection from "~/components/templates/home-page/hero-section.vue";
+  import HeroSection from "~~/components/templates/home-page/hero-section/hero-section.vue";
   import CompanyCarousel from "~~/components/templates/home-page/company-carousel/company-carousel.vue";
 </script>
 
