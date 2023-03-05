389e1f74be5607bddc2a2cbffada93150f3c0a44
Daniele Tortora <danieletortora.contact@gmail.com>
2023-03-05 22:29:05 +0100

    feat: add favicon and hackney logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,13 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "en",
       },
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/favicon.ico",
+        },
+      ],
       title: siteMetadata.pageTitle,
       meta: [
         { name: "title", content: siteMetadata.pageTitle },
