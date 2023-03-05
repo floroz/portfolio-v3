7dcc4aa639ec11582983e0e88587002e9411c4f9
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2023-03-05 19:12:46 +0100

    feat: site metadata and tags

---
+++ config/site-metadata.ts
@@ -0,0 +1,6 @@
+export default {
+  pageTitle: "Daniele Tortora | Software Engineer",
+  pageDescription:
+    "I am a highly skilled Software Engineer with a passion for creating rich user interfaces and interactive web applications. With a strong focus on usability, accessibility, and inclusiveness, I specialize in translating interaction designs and product concepts into immersive experiences for end users.",
+  siteUrl: "https://www.danieletortora.com",
+};
