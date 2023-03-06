ff4da188ecc36ffcc5082d2f31faef36275e0a0f
Daniele Tortora <tortora87@gmail.com>
2023-03-06 08:47:01 +0100

    feat: add GA4

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
   import BaseFooter from "~~/components/organisms/base-footer.vue";
   import BaseHeader from "~~/components/organisms/base-header.vue";
+
+  useHead({
+    script: [
+      {
+        src: "https://www.googletagmanager.com/gtag/js?id=G-86J1F6NHN7",
+        async: true,
+      },
+      {
+        innerHTML: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-86J1F6NHN7');
+      `,
+        type: "text/javascript",
+      },
+    ],
+  });
 </script>
 
 <template>
