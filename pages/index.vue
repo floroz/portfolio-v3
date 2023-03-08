970a78ec042eb4f4f74c887e099b6dff0dfe649e
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2023-03-08 13:39:01 +0100

    feat: add resume

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
   import HeroSection from "~~/components/templates/home-page/hero-section/hero-section.vue";
   import CompanyCarousel from "~~/components/templates/home-page/company-carousel/company-carousel.vue";
+  import AboutSection from "~~/components/templates/home-page/about-section/about-section.vue";
 </script>
 
 <template>
   <NuxtLayout>
     <HeroSection />
     <CompanyCarousel />
+    <AboutSection />
   </NuxtLayout>
 </template>
