af2d29a3c09f88f4deec71971e001d56620fa398
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2023-07-24 09:07:39 +0200

    feat: migrate resume.html to a nuxt page with tailwindcss

---
+++ components/molecules/work-section.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { WorkEntry } from '../../types/resume'
+defineProps<{
+    entry: WorkEntry
+  }>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-1 mb-4">
+    <div class="flex w-full justify-between">
+      <p class="font-bold uppercase">{{ entry.companyName }}</p>
+      <p class="font-bold">{{ entry.startDate }} - {{ entry.endDate }}</p>
+    </div>
+    <div class="flex w-full justify-between italic">
+      <p>{{ entry.title }}{{ entry.isContract ? " (Contract)" : `` }}</p>
+      <p>{{ entry.location }}</p>
+    </div>
+    <div class="mt-2 flex w-full justify-center">
+      <ul class="list flex flex-col gap-y-2 list-disc list-outside max-w-3xl">
+        <li
+        class="list-item text-sm"
+          v-for="desc in entry.description"
+          :key="desc"
+        >
+          {{ desc }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
