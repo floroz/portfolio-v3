54cb8dba670199e08fea2670a45651c951a3bc30
Daniele Tortora <tortora87@gmail.com>
2023-07-25 09:11:05 +0200

    feat: upgrade project to recent deps

--- components/molecules/work-section.vue
+++ components/molecules/work-section.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import type { WorkEntry } from '../../types/resume'
-defineProps<{
-    entry: WorkEntry
+  import type { WorkEntry } from "../../types/resume";
+  defineProps<{
+    entry: WorkEntry;
   }>();
 </script>
 
 <template>
-  <div class="flex flex-col gap-y-1 mb-4">
+  <div class="mb-4 flex flex-col gap-y-1">
     <div class="flex w-full justify-between">
       <p class="font-bold uppercase">{{ entry.companyName }}</p>
       <p class="font-bold">{{ entry.startDate }} - {{ entry.endDate }}</p>
@@ -16,11 +16,11 @@ defineProps<{
       <p>{{ entry.location }}</p>
     </div>
     <div class="mt-2 flex w-full justify-center">
-      <ul class="list flex flex-col gap-y-2 list-disc list-outside max-w-3xl">
+      <ul class="flex max-w-3xl list-outside list-disc flex-col gap-y-2">
         <li
-        class="list-item text-sm"
           v-for="desc in entry.description"
           :key="desc"
+          class="list-item text-sm"
         >
           {{ desc }}
         </li>
