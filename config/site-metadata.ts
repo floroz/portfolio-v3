9d75d110b545457ddf6db6a999ad5ea32986ddee
Daniele Tortora <daniele.tortora@frontiersin.org>
2024-01-18 15:23:09 +0100

    feat: update cv with missing technical aspects

--- config/site-metadata.ts
+++ config/site-metadata.ts
@@ -1,6 +1,6 @@
 export default {
   pageTitle: "Daniele Tortora | Technical Lead / Senior Frontend Engineer",
   pageDescription:
-    "I am a highly skilled Technical Lead and Senior Frontend Engineer with a proven track record of success in building rich user interfaces and interactive web applications. With over 6 years of experience, I specialize in translating interaction design and product concepts into immersive experiences that prioritize usability, accessibility, and inclusivity.",
+    "I am a Technical Lead and Senior Frontend Engineer with a proven track record of success in building rich user interfaces and interactive web applications. With over 6 years of experience, I specialize in translating interaction design and product concepts into immersive experiences that prioritize usability, accessibility, and inclusivity.",
   siteUrl: "https://www.danieletortora.com",
 };
