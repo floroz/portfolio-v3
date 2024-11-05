a36809cf3dd3e6c8473da504c6bcf3e8f6854ff9
Daniele Tortora <daniele.tortora@snyk.io>
2024-11-05 22:40:17 +0100

    chore: update text on hero and about

--- components/templates/home-page/about-section/about-section.vue
+++ components/templates/home-page/about-section/about-section.vue
@@ -21,51 +21,14 @@
       </BaseButtonLink>
       <div class="text-primary flex flex-col gap-y-8 text-lg">
         <p>
-          I am a Technical Lead and Senior Frontend Engineer with a proven track
-          record of success in building rich user interfaces and interactive web
-          applications. With over 7 years of experience, I specialize in
-          translating interaction design and product concepts into immersive
-          experiences that prioritize usability, accessibility, and inclusivity.
-        </p>
-
-        <p>
-          As a tech lead, I have driven successful front-end architecture,
-          implemented design systems, and created component libraries, resulting
-          in improved team efficiency and code quality. I have a passion for
-          promoting a healthy team culture that values clean, modular, and
-          loosely coupled code. By identifying and implementing suitable design
-          patterns, I ensure the creation of flexible and maintainable software
-          architectures.
-        </p>
-
-        <p>
-          I excel in JavaScript/TypeScript development and possess commercial
-          experience with all modern front-end frameworks such as React, Vue,
-          and Angular. My understanding of HTML and CSS semantics, along with
-          modern meta-frameworks like Next.js and Nuxt.js, allows me to solve
-          various business problems and cater to diverse user needs. I stay on
-          top of the latest trends and tools to continuously adapt and thrive in
-          the ever-evolving front-end landscape.
-        </p>
-
-        <p>
-          I am passionate about test automation and have helped teams enhance
-          their QA processes through effective testing strategies. My
-          proficiency includes tools such as Jest/Vitest, Cypress/Playwright,
-          Storybook, and Percy/Chromatic. By implementing robust testing
-          methodologies, I ensure the delivery of reliable and bug-free
-          software. Having adopted a DevOps mindset, I have successfully
-          improved continuous integration pipelines using platforms such as
-          GitHub Actions, CircleCI, and Jenkins. By streamlining development
-          processes, I have contributed to faster delivery cycles without
-          compromising code quality.
-        </p>
-
-        <p>
-          I take pride in being a team player and mentor. I have fostered a
-          healthy team culture that encourages collaboration and knowledge
-          sharing. By promoting clean code practices, I have empowered teams to
-          build software that is scalable, robust, and easier to maintain.
+          Software Engineer with 8 years of experience in frontend, full-stack,
+          and distributed systems. Skilled in TypeScript, Node.js, and frontend
+          frameworks like React, Vue, and Angular, with backend expertise across
+          REST APIs, gRPC, GraphQL, and integrating with storage solutions like
+          Redis, PostgreSQL, and S3. Experienced in both microservices using
+          Kubernetes, and monolithic architectures on cloud platforms such as
+          AWS and GCP, with a DevOps-oriented approach to observability,
+          performance monitoring, and deployment management
         </p>
       </div>
     </div>
